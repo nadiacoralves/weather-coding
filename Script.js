49dedd633dc9780284436ad4c7192d73586168dd
nadiacoralves <55755193+nadiacoralves@users.noreply.github.com>
2019-11-25 17:17:51 +0000

    functions rearrengement and icon addition

--- Script.js
+++ Script.js
@@ -20,65 +20,31 @@ let day = days[now.getDay()];
 
 h2.innerHTML = `${day},    ${hours}:${minutes}`;
 
-function convertToCelsius() {
-  let temperature = document.querySelector("#temperature");
-  let link = document.querySelector("#celsius");
-  link.classList.add("active");
-  let fahrenheit = document.querySelector("#fahrenheit");
-  fahrenheit.classList.remove("active");
-  celsius.classList.add("active");
-  temperature.innerHtml = 14;
-}
-
-function convertToFahrenheit() {
-  let temperature = document.querySelector("#temperature");
-  let link = document.querySelector("#fahrenheit");
-  link.classList.add("active");
-  let celsius = document.querySelector("#celsius");
-  celsius.classList.remove("active");
-  fahrenheit.classList.add("active");
-  temperature.innerHTML = Math.round((22 * 9) / 5 + 32);
-}
-
-function handleError(error) {
-  if (error.response.status === 404) {
-    alert("This city doesn't exist");
-  }
-}
-
-let celsiusLink = document.querySelector("#celsius");
-celsiusLink.addEventListener("click", convertToCelsius);
-convertToCelsius();
-
-let fahrenheitLink = document.querySelector("#fahrenheit");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
-
 function showTemperature(response) {
-  console.log(response.data);
-  let temperature = document.querySelector("#temperature");
-  temperature.innerHTML = `${Math.round(response.data.main.temp)}`;
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let pressureElement = document.querySelector("#pressure");
+  let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  pressureElement.innerHTML = response.data.main.pressure;
+  humidityElement.innerHTML = response.data.main.humidity;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
 }
 
-function search(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#searched-text");
+let apiKey = "c190f24c748e3ec4a42698da3696febd";
+let city = "New York";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-  let city = document.querySelector("#city");
-  if (searchInput.value) {
-    city.innerHTML = `${searchInput.value}`;
-  } else {
-    city.innerHTML = null;
-    alert("Please type a city name");
-  }
-
-  let apiKey = "c190f24c748e3ec4a42698da3696febd";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric`;
-
-  axios
-    .get(url)
-    .then(showTemperature)
-    .catch(handleError);
-}
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+axios
+  .get(url)
+  .then(showTemperature)
+  .catch(handleError);
